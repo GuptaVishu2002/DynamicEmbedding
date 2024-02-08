07dbf35aa0262c415a9062b3397ff4d6aa5f714d
GuptaVishu2002 <32040675+GuptaVishu2002@users.noreply.github.com>
2024-02-08 14:42:51 -0500

    Add files via upload

---
+++ GNN/roost/roost/data.py
@@ -0,0 +1,339 @@
+import functools
+import os
+import re
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from jarvis.core.atoms import Atoms
+
+from roost.core import LoadFeaturiser
+
+input256 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100', '101', '102', '103', '104', '105', '106', '107', '108', '109', '110', '111', '112', '113', '114', '115', '116', '117', '118', '119', '120', '121', '122', '123', '124', '125', '126', '127', '128', '129', '130', '131', '132', '133', '134', '135', '136', '137', '138', '139', '140', '141', '142', '143', '144', '145', '146', '147', '148', '149', '150', '151', '152', '153', '154', '155', '156', '157', '158', '159', '160', '161', '162', '163', '164', '165', '166', '167', '168', '169', '170', '171', '172', '173', '174', '175', '176', '177', '178', '179', '180', '181', '182', '183', '184', '185', '186', '187', '188', '189', '190', '191', '192', '193', '194', '195', '196', '197', '198', '199', '200', '201', '202', '203', '204', '205', '206', '207', '208', '209', '210', '211', '212', '213', '214', '215', '216', '217', '218', '219', '220', '221', '222', '223', '224', '225', '226', '227', '228', '229', '230', '231', '232', '233', '234', '235', '236', '237', '238', '239', '240', '241', '242', '243', '244', '245', '246', '247', '248', '249', '250', '251', '252', '253', '254', '255']
+
+class CompositionData(Dataset):
+    """
+    The CompositionData dataset is a wrapper for a dataset data points are
+    automatically constructed from composition strings.
+    """
+
+    def __init__(self, data_path, fea_path, task, fea_num):
+        """
+        """
+        assert os.path.exists(data_path), "{} does not exist!".format(data_path)
+        # NOTE make sure to use dense datasets, here do not use the default na
+        # as they can clash with "NaN" which is a valid material
+        self.df = pd.read_csv(data_path, keep_default_na=False, na_values=[])
+        self.fea_num = fea_num
+        assert os.path.exists(fea_path), "{} does not exist!".format(fea_path)
+        self.elem_features = LoadFeaturiser(fea_path)
+        self.elem_emb_len = self.elem_features.embedding_size
+        self.task = task
+        if self.task == "regression":
+            if self.df.shape[1] - 2 != 1:
+                raise NotImplementedError(
+                    "Multi-target regression currently not supported"
+                )
+            self.n_targets = 1
+        elif self.task == "classification":
+            if self.df.shape[1] - 2 != 1:
+                raise NotImplementedError(
+                    "One-Hot input not supported please use categorical integer"
+                    " inputs for classification i.e. Dog = 0, Cat = 1, Mouse = 2"
+                )
+            self.n_targets = np.max(self.df[self.df.columns[2]].values) + 1
+
+    def __len__(self):
+        return len(self.df)
+
+    @functools.lru_cache(maxsize=None)  # Cache loaded structures
+    def __getitem__(self, idx):
+        """
+
+        Returns
+        -------
+        atom_weights: torch.Tensor shape (M, 1)
+            weights of atoms in the material
+        atom_fea: torch.Tensor shape (M, n_fea)
+            features of atoms in the material
+        self_fea_idx: torch.Tensor shape (M*M, 1)
+            list of self indices
+        nbr_fea_idx: torch.Tensor shape (M*M, 1)
+            list of neighbor indices
+        target: torch.Tensor shape (1,)
+            target value for material
+        cry_id: torch.Tensor shape (1,)
+            input id for the material
+        """
+        cry_id, composition, target = self.df.iloc[idx]
+        poscar_path = '/data/vgf3011/alignntldata/jid/'
+        #poscar_path = '../../structgnn/expt1_alignn/'
+        atoms = Atoms.from_poscar(poscar_path + composition)
+        formula = atoms.composition.reduced_formula
+        elements, weights = parse_roost(formula)
+        weights = np.atleast_2d(weights).T / np.sum(weights)
+        sstring = '.vasp'
+        if str(composition).endswith(sstring):
+            comp_name = str(composition)[:-(len(sstring))]
+        feat_path = '/data/vgf3011/alignntldata/data_elem/'
+        #feat_path = '../../../alignn_new3/data/expt1/x/'
+        df_feat = pd.read_csv(feat_path + comp_name + '_{}.csv'.format(self.fea_num))
+        #assert len(elements) != 1, f"cry-id {cry_id} [{composition}] is a pure system"
+        try:
+            feat_list = []
+            for element in elements:
+                feat_list.append(np.asarray(df_feat.loc[df_feat['element'] == element][input256]))
+            atom_fea =  np.vstack(feat_list)
+            #atom_fea = np.vstack(
+            #    [self.elem_features.get_fea(element) for element in elements]
+            #)
+        except AssertionError:
+            raise AssertionError(
+                f"cry-id {cry_id} [{composition}] contains element types not in embedding"
+            )
+        except ValueError:
+            raise ValueError(
+                f"cry-id {cry_id} [{composition}] composition cannot be parsed into elements"
+            )
+
+        env_idx = list(range(len(elements)))
+        self_fea_idx = []
+        nbr_fea_idx = []
+        nbrs = len(elements) - 1
+        for i, _ in enumerate(elements):
+            self_fea_idx += [i] * nbrs
+            nbr_fea_idx += env_idx[:i] + env_idx[i + 1 :]
+
+        # convert all data to tensors
+        atom_weights = torch.Tensor(weights)
+        atom_fea = torch.Tensor(atom_fea)
+        self_fea_idx = torch.LongTensor(self_fea_idx)
+        nbr_fea_idx = torch.LongTensor(nbr_fea_idx)
+        if self.task == "regression":
+            targets = torch.Tensor([float(target)])
+        elif self.task == "classification":
+            targets = torch.LongTensor([target])
+
+        return (
+            (atom_weights, atom_fea, self_fea_idx, nbr_fea_idx),
+            targets,
+            formula,
+            cry_id,
+        )
+
+
+def collate_batch(dataset_list):
+    """
+    Collate a list of data and return a batch for predicting crystal
+    properties.
+
+    Parameters
+    ----------
+
+    dataset_list: list of tuples for each data point.
+      (atom_fea, nbr_fea, nbr_fea_idx, target)
+
+      atom_fea: torch.Tensor shape (n_i, atom_fea_len)
+      nbr_fea: torch.Tensor shape (n_i, M, nbr_fea_len)
+      self_fea_idx: torch.LongTensor shape (n_i, M)
+      nbr_fea_idx: torch.LongTensor shape (n_i, M)
+      target: torch.Tensor shape (1, )
+      cif_id: str or int
+
+    Returns
+    -------
+    N = sum(n_i); N0 = sum(i)
+
+    batch_atom_weights: torch.Tensor shape (N, 1)
+    batch_atom_fea: torch.Tensor shape (N, orig_atom_fea_len)
+        Atom features from atom type
+    batch_self_fea_idx: torch.LongTensor shape (N, M)
+        Indices of mapping atom to copies of itself
+    batch_nbr_fea_idx: torch.LongTensor shape (N, M)
+        Indices of M neighbors of each atom
+    crystal_atom_idx: list of torch.LongTensor of length N0
+        Mapping from the crystal idx to atom idx
+    target: torch.Tensor shape (N, 1)
+        Target value for prediction
+    batch_comps: list
+    batch_ids: list
+    """
+    # define the lists
+    batch_atom_weights = []
+    batch_atom_fea = []
+    batch_self_fea_idx = []
+    batch_nbr_fea_idx = []
+    crystal_atom_idx = []
+    batch_target = []
+    batch_comp = []
+    batch_cry_ids = []
+
+    cry_base_idx = 0
+    for i, (inputs, target, comp, cry_id) in enumerate(dataset_list):
+        atom_weights, atom_fea, self_fea_idx, nbr_fea_idx = inputs
+        # number of atoms for this crystal
+        n_i = atom_fea.shape[0]
+
+        # batch the features together
+        batch_atom_weights.append(atom_weights)
+        batch_atom_fea.append(atom_fea)
+
+        # mappings from bonds to atoms
+        batch_self_fea_idx.append(self_fea_idx + cry_base_idx)
+        batch_nbr_fea_idx.append(nbr_fea_idx + cry_base_idx)
+
+        # mapping from atoms to crystals
+        crystal_atom_idx.append(torch.tensor([i] * n_i))
+
+        # batch the targets and ids
+        batch_target.append(target)
+        batch_comp.append(comp)
+        batch_cry_ids.append(cry_id)
+
+        # increment the id counter
+        cry_base_idx += n_i
+
+    return (
+        (
+            torch.cat(batch_atom_weights, dim=0),
+            torch.cat(batch_atom_fea, dim=0),
+            torch.cat(batch_self_fea_idx, dim=0),
+            torch.cat(batch_nbr_fea_idx, dim=0),
+            torch.cat(crystal_atom_idx),
+        ),
+        torch.stack(batch_target, dim=0),
+        batch_comp,
+        batch_cry_ids,
+    )
+
+
+def format_composition(comp):
+    """ format str to ensure weights are explicate
+    example: BaCu3 -> Ba1Cu3
+    """
+    subst = r"\g<1>1.0"
+    comp = re.sub(r"[\d.]+", lambda x: str(float(x.group())), comp.rstrip())
+    comp = re.sub(r"([A-Z][a-z](?![0-9]))", subst, comp)
+    comp = re.sub(r"([A-Z](?![0-9]|[a-z]))", subst, comp)
+    comp = re.sub(r"([\)](?=[A-Z]))", subst, comp)
+    comp = re.sub(r"([\)](?=\())", subst, comp)
+    return comp
+
+
+def parenthetic_contents(string):
+    """
+    Generate parenthesized contents in string as (level, contents, weight).
+    """
+    num_after_bracket = r"[^0-9.]"
+
+    stack = []
+    for i, c in enumerate(string):
+        if c == "(":
+            stack.append(i)
+        elif c == ")" and stack:
+            start = stack.pop()
+            num = re.split(num_after_bracket, string[i + 1 :])[0] or 1
+            yield {
+                "value": [string[start + 1 : i], float(num), False],
+                "level": len(stack) + 1,
+            }
+
+    yield {"value": [string, 1, False], "level": 0}
+
+
+def splitout_weights(comp):
+    """ split composition string into elements (keys) and weights
+    example: Ba1Cu3 -> [Ba,Cu] [1,3]
+    """
+    elements = []
+    weights = []
+    regex3 = r"(\d+\.\d+)|(\d+)"
+    try:
+        parsed = [j for j in re.split(regex3, comp) if j]
+    except:
+        print("parsed:", comp)
+    elements += parsed[0::2]
+    weights += parsed[1::2]
+    weights = [float(w) for w in weights]
+    return elements, weights
+
+
+def update_weights(comp, weight):
+    """ split composition string into elements (keys) and weights
+    example: Ba1Cu3 -> [Ba,Cu] [1,3]
+    """
+    regex3 = r"(\d+\.\d+)|(\d+)"
+    parsed = [j for j in re.split(regex3, comp) if j]
+    elements = parsed[0::2]
+    weights = [float(p) * weight for p in parsed[1::2]]
+    new_comp = ""
+    for m, n in zip(elements, weights):
+        new_comp += m + f"{n:.2f}"
+    return new_comp
+
+
+class Node(object):
+    """ Node class for tree data structure """
+
+    def __init__(self, parent, val=None):
+        self.value = val
+        self.parent = parent
+        self.children = []
+
+    def __repr__(self):
+        return f"<Node {self.value} >"
+
+
+def build_tree(root, data):
+    """ build a tree from ordered levelled data """
+    for record in data:
+        last = root
+        for _ in range(record["level"]):
+            last = last.children[-1]
+        last.children.append(Node(last, record["value"]))
+
+
+def print_tree(current, depth=0):
+    """ print out the tree structure """
+    for child in current.children:
+        print("  " * depth + "%r" % child)
+        print_tree(child, depth + 1)
+
+
+def reduce_tree(current):
+    """ perform a post-order reduction on the tree """
+    if not current:
+        pass
+
+    for child in current.children:
+        reduce_tree(child)
+        update_parent(child)
+
+
+def update_parent(child):
+    """ update the str for parent """
+    input_str = child.value[2] or child.value[0]
+    new_str = update_weights(input_str, child.value[1])
+    pattern = re.escape("(" + child.value[0] + ")" + str(child.value[1]))
+    old_str = child.parent.value[2] or child.parent.value[0]
+    child.parent.value[2] = re.sub(pattern, new_str, old_str, 0)
+
+
+def parse_roost(string):
+    # format the string to remove edge cases
+    string = format_composition(string)
+    # get nested bracket structure
+    nested_levels = list(parenthetic_contents(string))
+    if len(nested_levels) > 1:
+        # reverse nested list
+        nested_levels = nested_levels[::-1]
+        # plant and grow the tree
+        root = Node("root", ["None"] * 3)
+        build_tree(root, nested_levels)
+        # reduce the tree to get compositions
+        reduce_tree(root)
+        return splitout_weights(root.children[0].value[2])
+
+    else:
+        return splitout_weights(string)
